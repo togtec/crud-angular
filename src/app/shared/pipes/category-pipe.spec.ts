ddd6ff7eebf4e37de9a0b6cbda2ebdc45c372b95
togtec <togtec.dev@gmail.com>
2026-05-08 12:52:57 -0300

    test(course): add unit tests for CategoryPipe

--- src/app/shared/pipes/category-pipe.spec.ts
+++ src/app/shared/pipes/category-pipe.spec.ts
@@ -1,8 +1,26 @@
 import { CategoryPipe } from './category-pipe';
 
-describe('CategoryPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryPipe();
-    expect(pipe).toBeTruthy();
+fdescribe('CategoryPipe', () => {
+  let pipe: CategoryPipe;
+
+  beforeEach(() => {
+    pipe = new CategoryPipe();
   });
+
+  it('should return code for Front-End category', () => {
+    expect(pipe.transform("Front-End")).toBe('code');
+    expect(pipe.transform("front-end")).toBe('code');
+    expect(pipe.transform("FRONT-END")).toBe('code');
+    expect(pipe.transform("FRONT-END")).toBe('code');
+    expect(pipe.transform("   Front-End   ")).toBe('code');
+  });
+
+  it('should return computer for Back-End category', () => {
+    expect(pipe.transform("Back-End")).toBe('computer');
+  });
+
+  it('should return error for unknown categories', () => {
+    expect(pipe.transform("Database")).toBe('error');
+  })
+
 });
--- src/app/shared/pipes/category-pipe.ts
+++ src/app/shared/pipes/category-pipe.ts
@@ -7,11 +7,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CategoryPipe implements PipeTransform {
 
   transform(value: string): string {
-    switch(value) {
-      case 'Front-End': return 'code';
-      case 'Back-End': return 'computer';
+    const normalizedValue = value.trim().toLowerCase();
+
+    switch(normalizedValue) {
+      case 'front-end':
+        return 'code';
+
+      case 'back-end':
+        return 'computer';
 
     }
+
     return 'error';
   }
 
